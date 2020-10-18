f42dea21be21a7cef311e43003c2dcdcf35ff033
Meet Vyas <meetvyas682@gmail.com>
2020-10-18 21:17:12 +0530

    Done

--- sample_data/prog4.asm
+++ sample_data/prog4.asm
@@ -1,8 +1,8 @@
 swap:
-    sll $v0, $a1, 2    
+    sll $v0, $a1, 2     
     add $v0, $a0, $v0   
     lw $t0, 0($v0)      
     lw $s0, 4($v0)      
     sw $s0, 0($v0)      
-    sw $t0, 4($v0)      
-    jr $ra  
+    sw $t0, 4($v0)     
+    jr $ra            
